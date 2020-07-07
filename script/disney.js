4cf90c9c1e43f3a1ccc49f604452403ee0d08d39
FireGGG <67953276+FireGGG@users.noreply.github.com>
2020-07-07 12:48:37 +0300

    Add files via upload

---
+++ script/disney.js
@@ -0,0 +1,33 @@
+var NumberOfWords = 1
+var words = new BuildArray(NumberOfWords)
+
+// Use the following variables to 
+// define your random words:
+words[1] = "https://todaynewspk.win/yVW1" 
+
+
+function BuildArray(size){
+this.length = size
+for (var i = 1; i <= size; i++){
+this[i] = null}
+return this
+}
+
+function PickRandomWord(frm) {
+// Generate a random number between 1 and NumberOfWords
+var rnd = Math.ceil(Math.random() * NumberOfWords)
+
+// Display the word inside the text box
+frm.WordBox.value = words[rnd]
+}
+
+function usersOnline() {
+  var rndUser;
+  rndUser = Math.floor(Math.random() * 50) + 100;
+  document.getElementsByClassName("wtf")[0].innerHTML ="Users online: " + rndUser;
+  setInterval(function(){
+    rndUser = Math.floor(Math.random() * 50) + 100;
+    document.getElementsByClassName("wtf")[0].innerHTML ="Users online: " + rndUser;
+  }, 5020);
+}
+
